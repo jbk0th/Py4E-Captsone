09e34bd6c93729ca9524554b0fa546ef9f30b5f8
jbk0th <jbkoth51@gmail.com>
2017-10-21 19:47:38 -0700

    Updated code documentation

--- Get_Data.py
+++ Get_Data.py
@@ -5,6 +5,8 @@ import re
 conn = sqlite3.connect("Heart_data.sqlite")
 cur = conn.cursor()
 
+# clear all tables before starting data acquistion
+
 cur.executescript('''
 DROP TABLE IF EXISTS Hospital_data;
 DROP TABLE IF EXISTS State;
@@ -18,6 +20,12 @@ DROP TABLE IF EXISTS Links;
 this many tables is overkill, all the ~2300 records could be put in the same table and work fine
 for the graphic id like to create. However, want experience setting up something more complex
 for experience'''
+
+#creation of all tables to be used in the database, separate tables were made
+#for any values that would be duplicated many time in a tables
+
+# The Links table serves as the linking table for all the records to JOIN on keys
+# and return the complete information for each row
 
 cur.execute('''CREATE TABLE IF NOT EXISTS Hospital_data (
             id INTEGER PRIMARY KEY, hospital_name TEXT,
@@ -33,8 +41,6 @@ cur.execute('''CREATE TABLE IF NOT EXISTS Payment_category (
 cur.execute('''CREATE TABLE IF NOT EXISTS Value_of_care (
             id INTEGER PRIMARY KEY, value_category TEXT UNIQUE)''')
 
-# Possbily change zip to an intger field, see how that will work when try to
-# make a graphic with it
 cur.execute('''CREATE TABLE IF NOT EXISTS Zip_code (
             id INTEGER PRIMARY KEY, zip_ INTEGER UNIQUE )''')
 
@@ -52,22 +58,21 @@ url_json = get_url.json()
 counter = 0
 
 # the data being input into the DB are appended with _dat, to avoid confusion with
-# the similarly named databased fields
+# the similarly named database fields
 for data in url_json['data']:
     if 'AMI' not in data[17]: continue
     hosp_name_dat = data[9]
     city_dat = data[11]
     state_dat = data[12]
-    # Possibly change this to an integer will see if allows graphic
     zip_dat = int(data[13])
     county_dat = data[14]
     pay_cat_dat = data[18]
     value_care_dat = data[26]
     denom_dat = data[19]
+    # The below statments remove any non decimal digit character, to return data ammenable to conversion to an integer type
     if len(re.sub('\D','',data[20])) <= 1:
         payment_dat = None
     else: payment_dat = int(re.sub('\D','',data[20]))
-            # need to convert this to an int currently has $
     if len(re.sub('\D','',data[21])) <= 1:
         lower_est_dat = None
     else: lower_est_dat = int(re.sub('\D','',data[21]))
@@ -79,6 +84,7 @@ for data in url_json['data']:
     #iteration counter to enable db commit to disk every 50 rows
     counter += 1
 
+    # Inserting all data in the appropriate table
     cur.execute('''INSERT OR IGNORE INTO  Hospital_data
                 (hospital_name, city, county, denominator, payment, lower_est, higher_est, lat, lon) VALUES (?,?,?,?,?,?,?,?,?)''',
                 (hosp_name_dat, city_dat, county_dat, denom_dat, payment_dat, lower_est_dat, higher_est_dat, lat_dat, lon_dat))
@@ -87,8 +93,9 @@ for data in url_json['data']:
     cur.execute('INSERT OR IGNORE INTO Value_of_care (value_category) VALUES (?)', (value_care_dat,))
     cur.execute('INSERT OR IGNORE INTO Zip_code (zip_) VALUES (?)', (zip_dat,))
 
-    '''Below accessing all the input record ids to construst links table when want to acess complete row info for a hopistal_Data entry'''
-#    try:
+    '''Below accessing all the input record ids to construst links table
+    to acess complete row info for a hosptal_Data entry'''
+
     cur.execute('SELECT id FROM Hospital_data WHERE hospital_name=? AND city=?',(hosp_name_dat, city_dat)) #understand the limit 1 is superfluous if the entry is truly unique
     hosp_id_dat = cur.fetchone()[0]
 
@@ -107,12 +114,13 @@ for data in url_json['data']:
     cur.execute('''INSERT OR IGNORE INTO Links
     (hosp_id, state_id, pay_id, val_id, zip_id) VALUES (?, ?, ?, ?, ?)''',
     (hosp_id_dat, state_id_dat, pay_id_dat, val_id_dat, zip_id_dat))
-    # except:
-    #     print('Error selecting table ids for'.format(hosp_name_dat))
-    # commit cached data to disk every 50 row reads
+
+    # commit the data to disk every 50 iterations
     if counter % 50 == 0:
         conn.commit()
         print('{} records committed thus far! Now on state {}'.format(counter, state_dat))
+
+#Final commit to catch all records not logged before close the DB connection
 conn.commit()
 print('{} records committed thus far! Now on state {}'.format(counter, state_dat))
 
@@ -151,5 +159,6 @@ LEFT JOIN Value_of_care ON Links.val_id = Value_of_care.id
 --
 LEFT JOIN Zip_code ON Links.zip_id = Zip_code.zip_
 --
+--- Example of a wonky record edge case WHERE Hospital_data.id=2695
 LIMIT 5
 '''
